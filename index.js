956be7550b8add72732475139a57f6ff50e8ef41
jaimeenkoffeekodes <149799052+jaimeenkoffeekodes@users.noreply.github.com>
2024-09-20 14:48:31 +0530

    minor : delete

--- index.js
+++ index.js
@@ -76,9 +76,7 @@ app.delete('/user_delete', async (req, res) => {
 
         return res.status(200).json({ message: 'User deleted successfully', deletedUser });
     } catch (error) {
-        if (error.code === 'P2025') {
-            return res.status(404).json({ message: 'User not found' });
-        }
+
         return res.status(500).json({ message: 'An error occurred while deleting the user', error });
     }
 });
