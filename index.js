92a3f6a934f49f7944d9a28f8a7765f0251381ca
jaimeenkoffeekodes <149799052+jaimeenkoffeekodes@users.noreply.github.com>
2024-09-20 14:48:00 +0530

    minor change

--- index.js
+++ index.js
@@ -71,13 +71,12 @@ app.delete('/user_delete', async (req, res) => {
 
     try {
         const deletedUser = await prisma.users.delete({
-            where: { id: Number(id) }, // Make sure to convert id to number if it's not
+            where: { id: Number(id) },
         });
 
         return res.status(200).json({ message: 'User deleted successfully', deletedUser });
     } catch (error) {
         if (error.code === 'P2025') {
-            // User not found
             return res.status(404).json({ message: 'User not found' });
         }
         return res.status(500).json({ message: 'An error occurred while deleting the user', error });
